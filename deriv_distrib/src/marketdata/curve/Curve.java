4f939e950f56ac9f07c8b90fda1ee5d529f5b42b
mattwhitmore <mattwhitmore@gmail.com>
2013-01-31 21:14:54 -0500

    more useful definition of a curve

--- deriv_distrib/src/marketdata/curve/Curve.java
+++ deriv_distrib/src/marketdata/curve/Curve.java
@@ -1,10 +1,15 @@
 package marketdata.curve;
 
 import java.util.Date;
+import java.util.Vector;
 
 import marketdata.MarketData;
 
 public interface Curve extends MarketData {
 	public Date getDate();
-	public double getDF();
+	public double getValue();
+	public Vector<Date> getPointDates();
+	public Vector<Double> getPointValues();
+	public void setPoints(Vector<Date> dates, Vector<Double> values);
+	public String getInterpolatorType();
 }
